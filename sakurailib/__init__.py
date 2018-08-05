f5a96ad80778dc8eefd6e78d35cc10351686fac6
kyle <kairu_madigan@yahoo.co.jp>
2018-08-05 13:10:33 +0900

    feat: 文本长度计数器（半全角）
    计算文本的显示长度，半角为1，全角为2

---
+++ sakurailib/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Author  : Kyle
+# @License : 
+# @Contact : kairu_madigan@yahoo.co.jp
+# @Date    : 2018/08/05 12:55
+
---
+++ sakurailib/text.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Author  : Kyle
+# @License : 
+# @Contact : kairu_madigan@yahoo.co.jp
+# @Date    : 2018/08/05 12:56
+
+import unicodedata
+
+
+def get_east_asian_width_count(text: str) -> int:
+    """获取文本的长度
+
+    判断每个文字的属于全角或是半角，半角的文字长度为1，全角文字长度为2，
+    然后将长度累加。
+
+    Args:
+        text: 需要获取长度的文本
+
+    Return:
+        文本长度，int类型
+    """
+    count = 0
+    for char in text:
+        # 半角为: H, Na, N, 全角为: F, W, A
+        if unicodedata.east_asian_width(char) in "FWA":
+            count += 2
+        else:
+            count += 1
+    return count
